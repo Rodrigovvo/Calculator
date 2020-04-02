018ace35523ee94f0753ad6fea7b8635907f0514
Rodrigo Vinícius <46830262+Rodrigovvo@users.noreply.github.com>
2020-04-01 22:11:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#coding: utf-8
+#author: Rodrigo Vinicius
+
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+class CalcGridLayout(GridLayout):
+    padding = (20, 20, 20, 20)
+    spacing: (5, 5)
+    def Calculo(self, calcular):
+        if calcular:
+            try:
+                self.display.text = str(eval(calcular))
+            except Exception:
+                self.display.text = "Error"
+
+class CalculadoraApp(App):
+    title = "Calculadora - Python_Kivy"
+    def build(self):
+        return CalcGridLayout()
+
+CalculadoraApp().run()
